6a0992d0a213e33dfe5e04963386bdd6af307b48
SvenBouwen <sven.bouwen@vdabcampus.be>
2022-08-31 17:03:04 +0200

    Adres

--- src/main/java/be/vdab/frituurfrida/controllers/IndexController.java
+++ src/main/java/be/vdab/frituurfrida/controllers/IndexController.java
@@ -1,5 +1,7 @@
 package be.vdab.frituurfrida.controllers;
 
+import be.vdab.frituurfrida.domain.Adres;
+import be.vdab.frituurfrida.domain.Gemeente;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -13,7 +15,11 @@ class IndexController {
     @GetMapping("/")
     public ModelAndView index() {
         var openOfGesloten = LocalDate.now().getDayOfWeek().equals(DayOfWeek.MONDAY) ? "gesloten" : "open";
-        return new ModelAndView("index", "openGesloten", openOfGesloten);
+        var modelAndView = new ModelAndView("index", "openGesloten", openOfGesloten);
+        modelAndView.addObject("adres",
+                new Adres("Koning Albertstraat", "4",
+                        new Gemeente("Nijlen", 2560)));
+        return modelAndView;
     }
 }
 
