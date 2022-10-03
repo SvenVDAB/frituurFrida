d9785280d184970b1ca7fca77f35051d4f542922
SvenBouwen <sven.bouwen@vdabcampus.be>
2022-10-03 21:25:27 +0200

    Repository

---
+++ src/main/java/be/vdab/frituurfrida/exceptions/SnackNietGevondenException.java
@@ -0,0 +1,5 @@
+package be.vdab.frituurfrida.exceptions;
+
+public class SnackNietGevondenException extends RuntimeException {
+    public static final long serialVersionUID = 1L;
+}
---
+++ src/main/java/be/vdab/frituurfrida/repositories/SnackRepository.java
@@ -0,0 +1,60 @@
+package be.vdab.frituurfrida.repositories;
+
+import be.vdab.frituurfrida.domain.Snack;
+import be.vdab.frituurfrida.exceptions.SnackNietGevondenException;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class SnackRepository {
+    private final JdbcTemplate template;
+
+    public SnackRepository(JdbcTemplate template) {
+        this.template = template;
+    }
+
+    private final RowMapper<Snack> snackRowMapper =
+            (result, rowNum) ->
+                    new Snack(result.getLong("id"),
+                            result.getString("naam"),
+                            result.getBigDecimal("prijs"));
+
+    public Optional<Snack> findById(long id) {
+        try {
+            var sql = """
+                    select id, naam, prijs
+                    from snacks where id = ?
+                    """;
+            return Optional.of(template.queryForObject(sql, snackRowMapper, id));
+        } catch (IncorrectResultSizeDataAccessException ex) {
+            return Optional.empty();
+        }
+    }
+
+    public void update(Snack snack) {
+        var sql = """
+                update snacks
+                set naam = ?, prijs = ?
+                where id = ?
+                """;
+        if (template.update(sql,
+                snack.getNaam(), snack.getPrijs(),
+                snack.getId()) == 0) {
+            throw new SnackNietGevondenException();
+        }
+    }
+
+    public List<Snack> findByBeginNaam(String beginNaam) {
+        var sql = """
+                select id, naam, prijs
+                from snacks
+                where naam like '"""
+                + beginNaam + "%'";
+        return template.query(sql, snackRowMapper, beginNaam);
+    }
+}
