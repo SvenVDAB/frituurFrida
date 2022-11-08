9f03eab648e984cab4e9edce88b5431ebaf1c117
SvenBouwen <sven.bouwen@vdabcampus.be>
2022-11-08 10:16:46 +0100

    Snack wijzigen

--- src/main/java/be/vdab/frituurfrida/controllers/SnackController.java
+++ src/main/java/be/vdab/frituurfrida/controllers/SnackController.java
@@ -1,15 +1,20 @@
 package be.vdab.frituurfrida.controllers;
 
+import be.vdab.frituurfrida.domain.Snack;
+import be.vdab.frituurfrida.exceptions.SnackNietGevondenException;
 import be.vdab.frituurfrida.forms.BeginNaamForm;
 import be.vdab.frituurfrida.services.SnackService;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("snacks")
@@ -50,5 +55,33 @@ public class SnackController {
         if (errors.hasErrors()) return modelAndView;
         return modelAndView.addObject("snacks",
                 snackService.findByBeginNaam(form.begin()));
+    }
+
+    @GetMapping("wijzigen/form/{id}")
+    public ModelAndView wijzigenForm(@PathVariable Long id) {
+        var modelAndView = new ModelAndView("wijzigSnack");
+        snackService.read(id).ifPresent(snack -> {
+            modelAndView.addObject("snack", snack);
+        });
+        return modelAndView;
+    }
+
+    @PostMapping("wijzigen")
+    public String wijzigen(@Valid Snack snack, Errors errors,
+                           RedirectAttributes redirect) {
+        if (errors.hasErrors()) {
+            //return new ModelAndView("wijzigSnack");
+            return "wijzigSnack";
+        }
+        try {
+            snackService.update(snack);
+/*        return new ModelAndView("snackAlfabet",
+                "alfabet", alfabet);*/
+            //return new ModelAndView("/");
+            return "redirect:/";
+        } catch (SnackNietGevondenException ex) {
+            redirect.addAttribute("snackNietGevonden", snack.getId());
+            return "redirect:/";
+        }
     }
 }
