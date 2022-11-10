6aa0046b83e163a5f735c6da03a01b4a0b958bb2
SvenBouwen <sven.bouwen@vdabcampus.be>
2022-11-10 14:31:41 +0100

    probleem pagina zoekdefriet regel/8-9

---
+++ src/main/java/be/vdab/frituurfrida/controllers/FrietController.java
@@ -0,0 +1,35 @@
+package be.vdab.frituurfrida.controllers;
+
+import be.vdab.frituurfrida.sessions.ZoekDeFriet;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("frieten")
+public class FrietController {
+    private final ZoekDeFriet zoekDeFriet;
+
+    public FrietController(ZoekDeFriet zoekDeFriet) {
+        this.zoekDeFriet = zoekDeFriet;
+    }
+
+    @GetMapping("zoeken")
+    public ModelAndView toonDeuren() {
+        return new ModelAndView("zoekDeFriet").addObject(zoekDeFriet);
+    }
+
+    @PostMapping("zoeken/opendeur")
+    public String openDeur(int index) {
+        zoekDeFriet.openDeur(index);
+        return "redirect:/frieten/zoeken";
+    }
+
+    @GetMapping("zoeken/nieuwspel")
+    public String nieuwSpel() {
+        zoekDeFriet.reset();
+        return "redirect:/frieten/zoeken";
+    }
+}
